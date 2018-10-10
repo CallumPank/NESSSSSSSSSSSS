bc887cbd783ac05cd498b2803d0c6459c071158a
Callum <callpank97@gmail.com>
2018-10-10 20:15:42 +0100

    Changing background colour

---
+++ NES/comp310.asm
@@ -0,0 +1,116 @@
+    .inesprg 1
+    .ineschr 1
+    .inesmap 0
+    .inesmir 1
+
+; ---------------------------------------------------------------------------
+
+PPUCTRL   = $2000
+PPUMASK   = $2001
+PPUSTATUS = $2002
+OAMDDR    = $2003
+OAMDATA   = $2004
+PPUSCROLL = $2005
+PPUADDR   = $2006
+PPUDATA   = $2007
+OAMDMA    = $4014
+
+    .bank 0
+    .org $C000
+
+; Initialisation code based on https://wiki.nesdev.com/w/index.php/Init_code
+RESET:
+    SEI        ; ignore IRQs
+    CLD        ; disable decimal mode
+    LDX #$40
+    STX $4017  ; disable APU frame IRQ
+    LDX #$ff
+    TXS        ; Set up stack
+    INX        ; now X = 0
+    STX PPUCTRL  ; disable NMI
+    STX PPUMASK  ; disable rendering
+    STX $4010  ; disable DMC IRQs
+
+    ; Optional (omitted):
+    ; Set up mapper and jmp to further init code here.
+
+    ; If the user presses Reset during vblank, the PPU may reset
+    ; with the vblank flag still true.  This has about a 1 in 13
+    ; chance of happening on NTSC or 2 in 9 on PAL.  Clear the
+    ; flag now so the vblankwait1 loop sees an actual vblank.
+    BIT PPUSTATUS
+
+    ; First of two waits for vertical blank to make sure that the
+    ; PPU has stabilized
+vblankwait1:
+    BIT PPUSTATUS
+    BPL vblankwait1
+
+    ; We now have about 30,000 cycles to burn before the PPU stabilizes.
+    ; One thing we can do with this time is put RAM in a known state.
+    ; Here we fill it with $00, which matches what (say) a C compiler
+    ; expects for BSS.  Conveniently, X is still 0.
+    TXA
+clrmem:
+    STA $000,x
+    STA $100,x
+    STA $300,x
+    STA $400,x
+    STA $500,x
+    STA $600,x
+    STA $700,x  ; Remove this if you're storing reset-persistent data
+
+    ; We skipped $200,x on purpose.  Usually, RAM page 2 is used for the
+    ; display list to be copied to OAM.  OAM needs to be initialized to
+    ; $EF-$FF, not 0, or you'll get a bunch of garbage sprites at (0, 0).
+
+    INX
+    BNE clrmem
+
+    ; Other things you can do between vblank waits are set up audio
+    ; or set up other mapper registers.
+
+vblankwait2:
+    BIT PPUSTATUS
+    BPL vblankwait2
+
+    ; End of initialisation code -
+
+    ;Resets the PPU
+    LDA PPUSTATUS
+
+    ; Writing address $3F10 to the PPU which deals with background colour
+    LDA #$3F
+    STA PPUADDR
+    LDA #$10
+    STA PPUADDR
+
+
+    ;Wrting the background colour
+    LDA #$0d
+    STA PPUDATA
+    ;enter an infinite loop
+
+
+forever:
+    JMP forever
+
+; ---------------------------------------------------------------------------
+
+; NMI is called on every frame
+NMI:
+    RTI         ; Return from interrupt
+
+; ---------------------------------------------------------------------------
+
+    .bank 1
+    .org $FFFA
+    .dw NMI
+    .dw RESET
+    .dw 0
+
+; ---------------------------------------------------------------------------
+
+    .bank 2
+    .org $0000
+    ; TODO: add graphics
---
+++ NES/comp310.nes.0.nl
@@ -0,0 +1,6 @@
+$C017#vblankwait1#
+$C035#vblankwait2#
+$C01D#clrmem#
+$C000#RESET#
+$C04F#NMI#
+$C04C#forever#
